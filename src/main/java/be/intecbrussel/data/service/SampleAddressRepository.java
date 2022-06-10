780cc4c16fc5096ce8d11ce7255231077c8aad0d
start.vaadin.com <no-reply@vaadin.com>
2022-06-10 08:44:28 +0000

    Generated project

---
+++ src/main/java/be/intecbrussel/data/service/SampleAddressRepository.java
@@ -0,0 +1,9 @@
+package be.intecbrussel.data.service;
+
+import be.intecbrussel.data.entity.SampleAddress;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SampleAddressRepository extends JpaRepository<SampleAddress, UUID> {
+
+}
